50ed696d5a6bbad48ea05362a2ae9078f95400b0
Justin Moen <jamoen7@gmail.com>
2018-10-31 20:51:37 -0500

    Refactor

--- test/main.py
+++ test/main.py
@@ -1,7 +1,7 @@
 import tcc
 
 
-def test():
+def main():
     node = tcc.bip32.from_seed(b"foo", "secp256k1")
     message = "hi"
     private_key = node.private_key()
@@ -9,5 +9,8 @@ def test():
     message_digest = tcc.sha256(message).digest()
     sig = tcc.secp256k1.sign(private_key, message_digest)
     verifies = tcc.secp256k1.verify(public_key, sig, message_digest)
-    print(verifies)
+    print("Verifies?", verifies)
 
+
+if __name__ == "__main__":
+    main()
