1cfe885e530848876bd3b291191a2e41cb3c6e2c
Justin Moen <jamoen7@gmail.com>
2019-01-02 23:44:23 -0600

    Refactor test folder to print to UI

--- test/main.py
+++ test/main.py
@@ -1,16 +1,25 @@
+import m5stack
 import tcc
 
 
-def main():
+def sign_and_verify():
     node = tcc.bip32.from_seed(b"foo", "secp256k1")
-    message = "hi"
     private_key = node.private_key()
     public_key = node.public_key()
+    message = "hi"
     message_digest = tcc.sha256(message).digest()
     sig = tcc.secp256k1.sign(private_key, message_digest)
     verifies = tcc.secp256k1.verify(public_key, sig, message_digest)
-    print("Verifies?", verifies)
+    return verifies
+
+
+def main():
+    tft = m5stack.Display()
+    verifies = sign_and_verify
+    message = "verifies? %s" % verifies
+    tft.text(tft.CENTER, 45, "verifies")
 
 
 if __name__ == "__main__":
     main()
+ 
