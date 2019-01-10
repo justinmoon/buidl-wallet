f691117846e00fd577d24408f178c8b187a7fac1
Gustavo Silva <gustavo.silva@aptoide.com>
2019-01-10 17:28:26 +0000

    [new] guess the word

--- ui/main.py
+++ ui/main.py
@@ -10,12 +10,20 @@ DISPLAY_MENU_STEP = 4
 END_STEP = 4
 
 AVAILABLE_MNEMONICS = {
-    '12': 'together strong topple gown hospital people pig spend sport sweet choose solve',
-    '18': 'when pool song man agree when hair ring undo weapon art number phone recycle journey drill word inherit',
-    '24': 'tonight slim actress bargain wrestle debris warfare flight switch hero forget flip exercise put retire screen organ wisdom sick banner bench mask key identify',
+    '12': 'innocent tumble guess quiz secret bean hello novel naive observe pull manage',
+    '18': 'divorce right carpet pair cute spatial truck labor nest case pet final empower wall taste call wrong gain',
+    '24': 'disagree world flower hen case gym emotion club champion fire web false ring scorpion zoo snake disagree genuine monkey sauce moon evoke method orchard',
+}
+
+AVAILABLE_PUBLIC_ADDRESSES = {
+    '12': '12MPjyhKLEDZk1drUuk9oi21ZwFbrTPqGQ',
+    '18': '1JGgnjyHK4C6xvsQFk81iVCXudwx7Faedj',
+    '24': '1BQ9ttuoCwpswMwEeNQMMM7Lqk6tBdZVVk',
 }
 
 mnemonic = ''
+address = ''
+guessed_word = ''
 lock = True
 
 # starts on step 0
@@ -54,23 +62,33 @@ def set_mnemonic(val):
     global mnemonic
     mnemonic = val
 
+def set_address(val):
+    global address
+    address = val
+
 def button_handler_a(pin, pressed):
     # if button was pressed
     if pressed is True:
         if active_step == GENERATE_MNEMONIC_STEP:
             set_mnemonic(AVAILABLE_MNEMONICS['12'])
+            set_address(AVAILABLE_PUBLIC_ADDRESSES['12'])
             unlock()
         elif active_step == SHOW_MNEMONIC_STEP:
             unlock()
+        elif active_step == GUESS_WORD_STEP:
+            guess_word(error = True)
 
         print("Button A pressed")
 
 
 def button_handler_b(pin, pressed):
     # if button was released
-    if pressed is False:
+    if pressed is True:
         if active_step == GENERATE_MNEMONIC_STEP:
             set_mnemonic(AVAILABLE_MNEMONICS['18'])
+            set_address(AVAILABLE_PUBLIC_ADDRESSES['18'])
+            unlock()
+        elif active_step == GUESS_WORD_STEP:
             unlock()
 
 
@@ -78,10 +96,13 @@ def button_handler_b(pin, pressed):
 
 def button_handler_c(pin, pressed):
     # if button was released
-    if pressed is False:
+    if pressed is True:
         if active_step == GENERATE_MNEMONIC_STEP:
             set_mnemonic(AVAILABLE_MNEMONICS['24'])
+            set_address(AVAILABLE_PUBLIC_ADDRESSES['24'])
             unlock()
+        elif active_step == GUESS_WORD_STEP:
+            guess_word(error = True)
 
         print("Button C pressed")
 
@@ -111,15 +132,28 @@ def show_mnemonic():
     print_button_a('Continue')
 
 
-def guess_word():
+def guess_word(error = False):
     clean_screen()
-    print_line_1("WORK IN PROGRESS")
+    print_line_1("Choose the 5th word:")
+
+    arr_mnemonic = mnemonic.split(' ')
+    print_button_a(arr_mnemonic[1])
+    print_button_b(arr_mnemonic[4])
+    print_button_c(arr_mnemonic[9])
+
+    if error:
+        print_line_2("Wrong answer!")
 
 def display_menu():
-    pass
+    clean_screen()
+    print_line_1("Address:")
+    print_line_2(address)
+    print_button_a('Save')
 
 def save():
-    pass
+    clean_screen()
+    print_line_1("Saved!!")
+    #TODO save the address
 
 def clean_screen():
     clear = '                                               '
